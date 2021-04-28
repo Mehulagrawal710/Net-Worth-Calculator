85b1f3f4eb4df46ee154ee24f686550fcb91dea3
Mehulagrawal710 <mehulagrawal710@gmail.com>
2021-04-28 22:09:47 +0530

    Add mongoDB silmilar DB - TinyDB

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, send_file, request,render_template, make_response, redirect
 from werkzeug.utils import secure_filename
 import os
-import pickle
 import dbconnect
 
 app = Flask(__name__)
@@ -10,43 +9,44 @@ app.config['UPLOAD_FOLDER'] = "uploads"
 app.config['MAX_CONTENT_LENGTH'] = 20 * 1024 * 1024  # 20 MB
 
 userDB = dbconnect.UserDB()
-financeDB = dbconnect.FinanceDB()
+accountDB = dbconnect.AccountDB()
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-	return render_template('index.html', baseurl = request.host_url, loginResponse = {}, signupResponse = {})
+	return render_template('index.html', baseurl = request.host_url, loginResponse = {"msg": "Welcome", "class": "primary"}, signupResponse = {"msg": "Welcome", "class": "primary"}, formToOpen = "login")
 
 @app.route('/<username>', methods=['GET', 'POST'])
 def getData(username):
 	if request.method == 'GET':
-		data = financeDB.get(username)
+		data = accountDB.get(username)
 		return render_template('main.html', baseurl = request.host_url, username =  username, data = data)
 	if request.method == 'POST':
-		entryType = request.form.get("entryType")
-		name = request.form.get("name")
+		category = request.form.get("category")
+		title = request.form.get("title")
 		amount = int(request.form.get("amount"))
 		entry = {
-			entryType : {
-				"name": name,
-				"amount": amount
-			}
+			"username": username,
+			"category": category,
+			"title": title,
+			"amount": amount
 		}
-		financeDB.save(username, entry)
+		accountDB.save(entry)
 		return redirect(username)
 
 @app.route('/signup', methods=['POST'])
 def signup():
 	username = request.form.get('username')
 	password = request.form.get('password')
+	email = request.form.get('email')
 	if userDB.exists(username):
-		return render_template('index.html', baseurl = request.host_url, loginResponse = {}, signupResponse = {"msg": "Username Already Exists", "color": "red"})
+		return render_template('index.html', baseurl = request.host_url, loginResponse = {"msg": "Welcome", "class": "primary"}, signupResponse = {"msg": "There is already a user registered with this username. Kindly choose another one", "class": "warning"}, formToOpen = "signup")
 	user = {
 		"username": username,
-		"password": password
+		"password": password,
+		"email": email
 	}
 	userDB.save(user)
-	financeDB.initialize(username)
-	return render_template('index.html', baseurl = request.host_url, loginResponse = {}, signupResponse = {"msg": "signup Successful, you can login now", "color": "green"})
+	return render_template('index.html', baseurl = request.host_url, loginResponse = {"msg": "Welcome", "class": "primary"}, signupResponse = {"msg": "signup Successful, you can login now", "class": "success"}, formToOpen = "signup")
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -54,11 +54,11 @@ def login():
 	password = request.form.get('password')
 	if userDB.exists(username):
 		user = userDB.get(username)
-		if user['password']==password:
+		if user["password"]==password:
 			return redirect(username)
 		else:
-			return render_template('index.html', baseurl = request.host_url, loginResponse = {"msg": "wrong password", "color": "red"}, signupResponse = {})
-	return render_template('index.html', baseurl = request.host_url, loginResponse = {"msg": "username not found", "color": "red"}, signupResponse = {})
+			return render_template('index.html', baseurl = request.host_url, loginResponse = {"msg": "wrong password", "class": "danger"}, signupResponse = {"msg": "Welcome", "class": "primary"}, formToOpen = "login")
+	return render_template('index.html', baseurl = request.host_url, loginResponse = {"msg": "username not found", "class": "danger"}, signupResponse = {"msg": "Welcome", "class": "primary"}, formToOpen = "login")
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=False)
--- dbconnect.py
+++ dbconnect.py
@@ -1,87 +1,52 @@
-import  pickle
+from tinydb import TinyDB, Query
+
+database_dir = "database/"
 
 class UserDB():
 	"""
-	User Object ->
-	username = {
-		username : "",
-		password: ""
+	{
+		"username": "mehul",
+		"password": "710",
+		"email": "mehulagrawal710@gmail.com"
 	}
 	"""
 	def __init__(self):
-		self.db_name = "database/userdb.pickle"
+		self.userDB = TinyDB(database_dir+"userdb.json")
 
 	def exists(self, username):
-		with open(self.db_name, 'rb') as handle:
-			db = pickle.load(handle)
-		if username in db.keys():
+		result = self.userDB.search(Query().username==username)
+		if result:
 			return True
 		return False
 
 	def save(self, user_object):
-		print("Saving user object :", user_object, "to database: ", self.db_name)
-		with open(self.db_name, 'rb') as handle:
-			db = pickle.load(handle)
-			username = user_object["username"]
-			db[username] = user_object
-		with open(self.db_name, 'wb') as handle:
-			pickle.dump(db, handle, protocol=pickle.HIGHEST_PROTOCOL)
-		print("Saved user object :", user_object, "to database:", self.db_name, "successfuly")
+		self.userDB.insert(user_object)
+		# self.userDB.purge()
+		print("===> Successfuly saved user: {}".format(user_object["username"]))
 
 	def get(self, username):
-		print("Fetching user object for username:", username)
-		with open(self.db_name, 'rb') as handle:
-			db = pickle.load(handle)
-		return db[username]
+		print("===> Fetching user object for username:".format(username))
+		result = self.userDB.search(Query().username==username)
+		return result[0]
 
-class FinanceDB():
+class AccountDB():
 	"""
-	Finance Object ->
-	username = {
-		username : "",
-		assets: [
-			{
-				name: ""
-				amount: ""
-			}
-		],
-		liabilities: [
-			{
-				name: ""
-				amount: ""
-			}
-		]
+	{
+		"username": "mehul",
+		"title": "house",
+		"category": "asset",
+		"amount": 1
 	}
 	"""
 	def __init__(self):
-		self.db_name = "database/financedb.pickle"
+		self.accountDB = TinyDB(database_dir+"accountdb.json")
 
-	def initialize(self, username):
-		with open(self.db_name, 'rb') as handle:
-			db = pickle.load(handle)
-			data = {
-				"username" : username,
-				"assets": [],
-				"liabilities": []
-			}
-			db[username] = data
-		with open(self.db_name, 'wb') as handle:
-			pickle.dump(db, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-	def save(self, username, entry):
-		print("Saving entry object :", entry, "to database: ", self.db_name)
-		with open(self.db_name, 'rb') as handle:
-			db = pickle.load(handle)
-			entryType = list(entry.keys())[0]
-			db[username][entryType].append(entry[entryType])
-		with open(self.db_name, 'wb') as handle:
-			pickle.dump(db, handle, protocol=pickle.HIGHEST_PROTOCOL)
-		print("Saved the entry :", entry, "to database:", self.db_name, "successfuly")
+	def save(self, entry_object):
+		self.accountDB.insert(entry_object)
+		# self.accountDB.purge()
+		print("===> Successfuly saved Entry for user: {}".format(entry_object["username"]))
 
 	def get(self, username):
-		print("Fetching finance object for username:", username)
-		with open(self.db_name, 'rb') as handle:
-			db = pickle.load(handle)
-		if username in db.keys():
-			return db[username]
-		return None
+		print("===> Fetching entry object for username:".format(username))
+		result = self.accountDB.search(Query().username==username)
+		return result
